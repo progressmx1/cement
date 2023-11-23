583a45ce455a433e1d89a17c572fc7e02e939326
progress munoriarwa <150421990+progressmx1@users.noreply.github.com>
2023-11-23 18:45:37 +0200

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+import withMT from "@material-tailwind/react/utils/withMT";
+ 
+module.exports = withMT({
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config);
